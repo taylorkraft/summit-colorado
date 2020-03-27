7eed3b7b30d36b1b89e8c98908d52f745af3c41a
taylorkraft <taylorkraft16@gmail.com>
2020-03-27 14:56:47 -0600

    edit controllers/seeds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,3 @@
-class UsersController < ApplicationController
+class UserController < ApplicationController
   
 end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Mountain.create(name:"Mount Evans", elevation:"14,265'", summited: true)
+Mountain.create(name:"Grays Peak", elevation:"14,278'", summited: true)
+Mountain.create(name:"Mount Massive", elevation:"14,429'", summited: false)
