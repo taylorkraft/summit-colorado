548c78a3fe559ff90b41874bf680008982eec152
taylorkraft <taylorkraft16@gmail.com>
2020-03-27 12:53:17 -0600

    routes set up

---
+++ app/controllers/mountains_controller.rb
@@ -0,0 +1,18 @@
+class MountainsController < ApplicationController
+
+  get '/mountains' do
+    
+  end
+
+  get '/mountains/new' do
+    
+  end
+
+  get '/mountains/:id' do
+    
+  end
+
+  post '/mountains' do
+    
+  end
+end
